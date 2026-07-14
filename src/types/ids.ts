fd10e8cec8867ff07e6bdf55198e341cc7c6a178
xiangtaizhongcun2-gif <xiangtaizhongcun2@gmail.com>
2026-07-14 12:05:41 +0900

    Add domain ID aliases

---
+++ src/types/ids.ts
@@ -0,0 +1,17 @@
+import type { ID } from "./base";
+
+export type GameId = ID;
+export type RuleSetId = ID;
+export type SeasonId = ID;
+export type TypeChartId = ID;
+export type PokemonSpeciesId = ID;
+export type PokemonKnowledgePageId = ID;
+export type PokemonBuildId = ID;
+export type BattleMechanicId = ID;
+export type PartyId = ID;
+export type BattleLogId = ID;
+export type SelectionMemoId = ID;
+export type ResearchNoteId = ID;
+export type DamageNoteId = ID;
+export type TagId = ID;
+export type UserSettingId = ID;
