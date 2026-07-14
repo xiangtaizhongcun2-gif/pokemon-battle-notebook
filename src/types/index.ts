504e6d790278ba9d79267b958ab9cd5177ec3f7d
xiangtaizhongcun2-gif <xiangtaizhongcun2@gmail.com>
2026-07-14 12:05:55 +0900

    Add type barrel exports

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+export type { BaseEntity, ID, ISODateString } from "./base";
+
+export type {
+  BattleLogId,
+  BattleMechanicId,
+  DamageNoteId,
+  GameId,
+  PartyId,
+  PokemonBuildId,
+  PokemonKnowledgePageId,
+  PokemonSpeciesId,
+  ResearchNoteId,
+  RuleSetId,
+  SeasonId,
+  SelectionMemoId,
+  TagId,
+  TypeChartId,
+  UserSettingId,
+} from "./ids";
