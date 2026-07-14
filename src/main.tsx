3cd7fbe286bd00fcedde205edddca91ac04cdaf7
xiangtaizhongcun2-gif <xiangtaizhongcun2@gmail.com>
2026-07-14 21:54:21 +0900

    Mount speed comparison panel

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,12 @@ import "./build-suggestions.css";
 import "./champions-training.css";
 import "./opponent-team-search.css";
 import "./data-backup.css";
+import "./speed-comparison.css";
 import { BattleStatistics } from "./simple/BattleStatistics";
 import { OpponentTeamSearch } from "./simple/OpponentTeamSearch";
 import { DataBackupManager } from "./simple/DataBackupManager";
 import { PartyCopyAction } from "./simple/PartyCopyAction";
+import { SpeedComparison } from "./simple/SpeedComparison";
 
 const rootElement = document.getElementById("root");
 
@@ -25,5 +27,6 @@ createRoot(rootElement).render(
     <BattleStatistics />
     <DataBackupManager />
     <PartyCopyAction />
+    <SpeedComparison />
   </StrictMode>,
 );
